f316892e4bb6c4896fbca4ccc0ea723a7cce56c8
chaiayapat100 <52157617+chaiayapat100@users.noreply.github.com>
2019-06-26 11:21:03 +0700

    Add files via upload

---
+++ FirstProgram.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<" Student Code : " << "62-06021620027 " << endl;
+	cout<<" Student Name : " << "Chaiyapat Nanawan " << endl;
+	cout<<" Division : " << " Information Technology " << endl;
+	cout<<" Year : " << " 1 " << endl;
+	cout<<" Room : " << " RA " << endl;
+	cout<<" Sex : " << " Male " << endl;
+	cout<<" Age :"<< " 18 " << endl;
+	cout<<"    #   "<< endl;
+	cout<<"   # #  "<< endl;
+	cout<<"  #   # "<< endl;
+	cout<<" #     # "<< endl;
+	cout<<" ####### "<< endl;
+	cout<<" #     # "<< endl;
+	return(0);
+}
+
+
